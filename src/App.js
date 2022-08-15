d907e7dfb4f3dadc419eb1c53dbfbdb10e743b32
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-15 09:38:38 -0700

    Hidden credentials

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
 import React, { useEffect } from 'react';
+import configData from './config.json';
 function App() {
   var allGames = {};
   useEffect(() => {
-    fetch('IPlayerService/GetOwnedGames/v1/?key=&steamid=76561198429268611&include_appinfo=true&include_played_free_games=true&appids_filter=0')
-      .then(res => res.json())
-      .then(res => {
-        for (var i = 0; i < res.response.game_count; i++) {
-          allGames[res.response.games[i].name] = res.response.games[i].appid;
-        }
-        console.log(res.response.game_count)
-        console.log('Output: ', allGames);
-        Object.entries(allGames).forEach(game => {
-        fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=&appid=' + game[1] + '&l=english&format=json')
-        .then(res => res.json())
-        .then(res => console.log(res))
-        })
-      });
+    // fetch('IPlayerService/GetOwnedGames/v1/?key=' + configData.apiKey + '&steamid=' + confidData.steamid + '&include_appinfo=true&include_played_free_games=true&appids_filter=0')
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     for (var i = 0; i < res.response.game_count; i++) {
+    //       allGames[res.response.games[i].name] = res.response.games[i].appid;
+    //     }
+    //     console.log(configData)
+    //     // console.log(res.response.game_count)
+    //     // console.log('Output: ', allGames);
+    //     Object.entries(allGames).forEach(game => {
+    //     fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game[1] + '&l=english&format=json')
+    //     .then(res => res.json())
+    //     .then(res => console.log(res))
+    //     })
+    //   });
   }, []
   );
 
