a42a13fcf3815957234b7bf24216b5fb0f1c0d42
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-18 15:32:45 -0700

    Changed name; moved API fetching elsewhere

---
+++ src/Game.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+const Game = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Here we are 
+      </header>
+    </div>
+  );
+}
+
+export default Game;
