70ec849802d51696a3c8f682d26e4cf29d385f60
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-18 15:49:13 -0700

    Markup cleanup

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import configData from './config.json';
 var allGames = {};
 // TODO: add below - it's used for each game component - links to game's guide
 // 'https://steamcommunity.com/app/' + <appid> + '/guides/'
+
 fetch('IPlayerService/GetOwnedGames/v1/?key=' + configData.apiKey + '&steamid=' + configData.steamId + '&include_appinfo=true&include_played_free_games=true&appids_filter=0')
   .then(res => res.json())
   .then(res => {
@@ -24,6 +25,7 @@ fetch('IPlayerService/GetOwnedGames/v1/?key=' + configData.apiKey + '&steamid=' 
   });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(allGames);
 root.render(
   <Game />
 );
