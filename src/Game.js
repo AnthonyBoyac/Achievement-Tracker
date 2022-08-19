e82ed1aa2283f5f3e6973e2605240b274c63d19e
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-19 10:39:10 -0700

    Removed console logs; removed unnecessary elements

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import './Game.css';
 const Game = (games) => {
   games = games.gameList.games;
   console.log(games);
   return (
-    <div>
+    <div className="game_container">
       {
         games.map((game) => {
-          console.log(game)
+          console.log(game);
           return (
-            <div className="game_container" key={game.appid}>
-              <header className="game_name">
-                Name: {game.name}
-              </header>
+            <div className="game" key={game.appid}>
+              Name: {game.name}
             </div>
           );
         })
