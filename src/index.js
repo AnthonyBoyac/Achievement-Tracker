442fa48f89cb15ecba136020d82a1210d650c4f0
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-19 10:24:11 -0700

    Added Promise object for fetch requests

--- src/index.js
+++ src/index.js
@@ -9,23 +9,25 @@ var allGames = {};
 // TODO: add below - it's used for each game component - links to game's guide
 // 'https://steamcommunity.com/app/' + <appid> + '/guides/'
 
-fetch('IPlayerService/GetOwnedGames/v1/?key=' + configData.apiKey + '&steamid=' + configData.steamId + '&include_appinfo=true&include_played_free_games=true&appids_filter=0')
+let fetchRequest = new Promise((resolve, reject) => {
+  fetch('IPlayerService/GetOwnedGames/v1/?key=' + configData.apiKey + '&steamid=' + configData.steamId + '&include_appinfo=true&include_played_free_games=true&appids_filter=0')
   .then(res => res.json())
   .then(res => {
-    for (var i = 0; i < res.response.game_count; i++) {
-      allGames[res.response.games[i].name] = res.response.games[i].appid;
-    }
-    console.log(res.response.game_count)
-    console.log('Output: ', allGames);
-    Object.entries(allGames).forEach(game => {
-    fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game[1] + '&l=english&format=json')
-    .then(res => res.json())
-    .then(res => console.log(res))
-    })
+    allGames = res.response;
+    // Object.entries(allGames).forEach(game => {
+    // fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game[1] + '&l=english&format=json')
+    // .then(res => res.json())
+    // .then(res => '')
+    // })
+    resolve();
   });
+})
+fetchRequest.then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <Game gameList={allGames}/>
+  );
+})
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(allGames);
-root.render(
-  <Game />
-);
+
+
