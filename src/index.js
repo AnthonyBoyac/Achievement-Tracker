ae4bc426086eeb55a2b7c3906cffcae1910ac1bd
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-20 09:42:55 -0700

    Fetch requests work; finding alternative for setTimeout

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import configData from './config.json';
 
 // store all games the user has with achievements enabled
 var allGames = {};
+var allGamesAchievements = [];
 // TODO: add below - it's used for each game component - links to game's guide
 // 'https://steamcommunity.com/app/' + <appid> + '/guides/'
 
@@ -14,18 +15,39 @@ let fetchRequest = new Promise((resolve, reject) => {
   .then(res => res.json())
   .then(res => {
     allGames = res.response;
-    // Object.entries(allGames).forEach(game => {
-    // fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game[1] + '&l=english&format=json')
-    // .then(res => res.json())
-    // .then(res => '')
+    // console.log(allGamesAchievements)
+    // allGames.games.forEach(game => {
+    //   fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game.appid + '&l=english&format=json')
+    //   .then(res => res.json())
+    //   .then(res => console.log(res))
     // })
-    resolve();
   });
+  setTimeout(() => {
+    allGames.games.forEach((game, index) => {
+      if (game.appid === 599390 || game.appid === 359550 || game.appid === 623990) {
+  
+      } else {
+        fetch('ISteamUserStats/GetPlayerAchievements/v0001/?appid=' + game.appid + '&key=' + configData.apiKey + '&steamid=' + configData.steamId)
+        .then(res => res.json())
+        .then(res => {
+          console.log(res)
+          allGamesAchievements.push(res.playerstats)
+        })
+      }
+  
+    })
+
+  }, 2000)
+  setTimeout(() => {
+    resolve();
+  }, 5000);
+
 })
 fetchRequest.then(() => {
+  console.log(allGamesAchievements)
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-    <Game gameList={allGames}/>
+    <Game gameList={allGames} gameAchievement={allGamesAchievements} />
   );
 })
 
