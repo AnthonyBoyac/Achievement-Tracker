8ea0ba74c79ca0f924766ee03c6258df6bcbb83d
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-20 09:41:57 -0700

    Progress on working to fetch requests async

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './Game.css';
-const Game = (games) => {
-  games = games.gameList.games;
-  console.log(games);
+const Game = (props) => {
+  let games = props.gameList.games;
+  let gamesAchievements = props.gameAchievement;
+  let index = 0;
+  console.log(games)
+  console.log(gamesAchievements)
   return (
     <div className="game_container">
       {
         games.map((game) => {
-          console.log(game);
-          return (
-            <div className="game" key={game.appid}>
-              Name: {game.name}
-            </div>
-          );
+          // TODO: can't use index because both lists are unordered, find another way
+          try {
+            console.log(gamesAchievements[index])
+            console.log(index)
+            let displayAchievements = gamesAchievements[index].achievements.forEach((achievement) => {
+              <div className="game_achievement">
+                {achievement.apiname}
+              </div>
+            })
+            index++;
+            return (
+              <div className="game" key={game.appid}>
+                Name: {game.name}
+                {displayAchievements}
+              </div>
+            );
+          } catch (error) {
+            console.log("gone too far")
+          }
+
         })
       }
 
