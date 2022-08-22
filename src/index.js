8eedb9e2ec1f85b315d28d4010c62326771920e3
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-08-22 12:13:09 -0700

    Managed to sort array with multiple fetch requests! WOOOO

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import Game from './Game';
 import configData from './config.json';
 
 // store all games the user has with achievements enabled
-var allGames = {};
-var allGamesAchievements = [];
+var gameList = [];
 // TODO: add below - it's used for each game component - links to game's guide
 // 'https://steamcommunity.com/app/' + <appid> + '/guides/'
 
@@ -14,24 +13,34 @@ let fetchRequest = new Promise((resolve, reject) => {
   fetch('IPlayerService/GetOwnedGames/v1/?key=' + configData.apiKey + '&steamid=' + configData.steamId + '&include_appinfo=true&include_played_free_games=true&appids_filter=0')
   .then(res => res.json())
   .then(res => {
-    allGames = res.response;
-    // console.log(allGamesAchievements)
-    // allGames.games.forEach(game => {
-    //   fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game.appid + '&l=english&format=json')
-    //   .then(res => res.json())
-    //   .then(res => console.log(res))
-    // })
+    res.response.games.forEach((game) => {
+      gameList.push({
+        gameName: game.name,
+        id: game.appid,
+        gameImage: game.img_icon_url,
+        achievements: []
+      })
+    })
+
+    gameList.forEach(game => {
+      fetch('ISteamUserStats/GetSchemaForGame/v0002/?key=' + configData.apiKey + '&appid=' + game.id + '&l=english&format=json')
+      .then(res => res.json())
+      .then(res => {
+        game.achievements = res.game.availableGameStats.achievements
+      })
+    })
   });
   setTimeout(() => {
-    allGames.games.forEach((game, index) => {
-      if (game.appid === 599390 || game.appid === 359550 || game.appid === 623990) {
+    gameList.forEach((game) => {
+      if (game.id === 599390 || game.id === 359550 || game.id === 623990) {
   
       } else {
-        fetch('ISteamUserStats/GetPlayerAchievements/v0001/?appid=' + game.appid + '&key=' + configData.apiKey + '&steamid=' + configData.steamId)
+        fetch('ISteamUserStats/GetPlayerAchievements/v0001/?appid=' + game.id + '&key=' + configData.apiKey + '&steamid=' + configData.steamId)
         .then(res => res.json())
         .then(res => {
-          console.log(res)
-          allGamesAchievements.push(res.playerstats)
+          game.achievements.forEach((achievement, index) => {
+            achievement.achieved = res.playerstats.achievements[index].achieved
+          })
         })
       }
   
@@ -40,14 +49,13 @@ let fetchRequest = new Promise((resolve, reject) => {
   }, 2000)
   setTimeout(() => {
     resolve();
-  }, 5000);
+  }, 6000);
 
 })
 fetchRequest.then(() => {
-  console.log(allGamesAchievements)
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-    <Game gameList={allGames} gameAchievement={allGamesAchievements} />
+    <Game gameList={gameList} />
   );
 })
 
